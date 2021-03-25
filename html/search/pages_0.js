5a73ec066db6979877f65ffb05b98fe829201da3
Lucas Umbelino <lucas-umbelino@hotmail.com>
2021-03-25 13:46:21 -0300

    Documentaçao

---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['escultor_5f3d_56',['Escultor_3D',['../md__d___p_r_o_g_r_a_m_a_c_a_o_arquivos__qt__projeto_escultor__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcegimnprsvw~",
+  1: "sv",
+  2: "mrs",
+  3: "cmpsw~",
+  4: "abginrv",
+  5: "e"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Classes",
+  2: "Arquivos",
+  3: "Funções",
+  4: "Variáveis",
+  5: "Páginas"
+};
+
